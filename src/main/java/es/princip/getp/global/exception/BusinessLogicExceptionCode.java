bc41dfb7a36423e63f6c60a5951541c0f0329ce7
Shin Changyu <scv1702@naver.com>
2024-01-08 23:55:55 +0900

    GETP-64 refactor: 테스트 코드 확장을 위해 Enum을 이용한 예외 처리에서 Class를 이용한 예외 처리로 변경

--- src/main/java/es/princip/getp/global/exception/BusinessLogicExceptionCode.java
+++
@@ -1,9 +0,0 @@
-package es.princip.getp.global.exception;
-
-import org.springframework.http.HttpStatus;
-
-public interface BusinessLogicExceptionCode {
-    public HttpStatus status();
-    public String code();
-    public String message();
-}
