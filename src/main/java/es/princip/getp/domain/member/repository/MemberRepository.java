c8fed4f7396279a01f24ea3e12dd60aef3bdacc5
Shin Changyu <scv1702@naver.com>
2024-01-11 13:56:08 +0900

    GETP-56 feat: 회원, 서비스 약관, 서비스 약관 동의 레포지토리 구현

--- src/main/java/es/princip/getp/domain/member/repository/MemberRepository.java
+++ src/main/java/es/princip/getp/domain/member/repository/MemberRepository.java
@@ -1,11 +1,8 @@
 package es.princip.getp.domain.member.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.princip.getp.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
-
-    Optional<Member> findByEmail(String email);
 }
