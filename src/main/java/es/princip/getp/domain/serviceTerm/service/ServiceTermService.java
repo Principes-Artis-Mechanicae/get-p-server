4a4bb979fc6142e9374863d984532a2184b99bbf
Shin Changyu <scv1702@naver.com>
2024-01-11 14:09:31 +0900

    GETP-56 feat: 서비스 약관 생성 API 구현

--- src/main/java/es/princip/getp/domain/serviceTerm/service/ServiceTermService.java
+++ src/main/java/es/princip/getp/domain/serviceTerm/service/ServiceTermService.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import es.princip.getp.domain.serviceTerm.dto.reqeust.ServiceTermRequest;
 import es.princip.getp.domain.serviceTerm.entity.ServiceTerm;
-import es.princip.getp.domain.serviceTerm.exception.ServiceTermErrorCode;
+import es.princip.getp.domain.serviceTerm.exception.ServiceTermNotFoundException;
 import es.princip.getp.domain.serviceTerm.repository.ServiceTermRepository;
-import es.princip.getp.global.exception.BusinessLogicException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,16 +15,16 @@ import lombok.RequiredArgsConstructor;
 public class ServiceTermService {
     private final ServiceTermRepository serviceTermRepository;
 
-    private ServiceTerm get(Optional<ServiceTerm> serviceTerm) {
-        return serviceTerm.orElseThrow(() -> new BusinessLogicException(ServiceTermErrorCode.SERVICE_TERM_NOT_FOUND));
+    private ServiceTerm resolve(Optional<ServiceTerm> serviceTerm) {
+        return serviceTerm.orElseThrow(() -> new ServiceTermNotFoundException());
     }
 
     public ServiceTerm getByServiceTermId(Long serviceTermId) {
-        return get(serviceTermRepository.findById(serviceTermId));
+        return resolve(serviceTermRepository.findById(serviceTermId));
     }
 
     public ServiceTerm getByTag(String tag) {
-        return get(serviceTermRepository.findByTag(tag));
+        return resolve(serviceTermRepository.findByTag(tag));
     }
 
     @Transactional
