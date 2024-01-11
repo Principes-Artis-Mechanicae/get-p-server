82c60fa81b7f1a001220da760aaab7b89aa9c9b5
Shin Changyu <scv1702@naver.com>
2024-01-11 13:45:34 +0900

    GETP-64 chore: Redis 테스트를 위한 testcontainers 설치

--- src/main/java/es/princip/getp/domain/auth/controller/SignUpController.java
+++ src/main/java/es/princip/getp/domain/auth/controller/SignUpController.java
@@ -8,37 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import es.princip.getp.domain.auth.dto.request.EmailVerificationCodeRequest;
 import es.princip.getp.domain.auth.dto.request.EmailVerificationRequest;
-import es.princip.getp.domain.auth.dto.request.SignUpRequest;
-import es.princip.getp.domain.auth.dto.response.SignUpResponse;
 import es.princip.getp.domain.auth.service.EmailVerificationService;
-import es.princip.getp.domain.auth.service.SignUpService;
 import es.princip.getp.global.util.ApiResponse;
-import es.princip.getp.global.util.ApiResponse.ApiSuccessResult;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth/signup")
 @RequiredArgsConstructor
 public class SignUpController {
-    private final SignUpService signUpService;
     private final EmailVerificationService emailVerificationService;
 
-    @PostMapping()
-    public ResponseEntity<ApiSuccessResult<SignUpResponse>> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
-        SignUpResponse signUpResponse = SignUpResponse.from(signUpService.signUp(signUpRequest));
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.CREATED, signUpResponse));
-    }
-
     @PostMapping("/email/send")
-    public ResponseEntity<ApiSuccessResult<?>> sendEmailVerificationCodeForSignUp(@RequestBody @Valid EmailVerificationCodeRequest verificationCodeRequest) {
+    public ResponseEntity<?> sendEmailVerificationCodeForSignUp(@RequestBody EmailVerificationCodeRequest verificationCodeRequest) {
         String email = verificationCodeRequest.email();
-        signUpService.sendEmailVerificationCodeForSignUp(email);
+        emailVerificationService.sendEmailVerificationCodeForSignUp(email);
         return ResponseEntity.ok().body(ApiResponse.success(HttpStatus.OK));
     }
 
     @PostMapping("/email/verify")
-    public ResponseEntity<ApiSuccessResult<?>> verifyEmail(@RequestBody @Valid EmailVerificationRequest verificationRequest) {
+    public ResponseEntity<?> verifyEmail(@RequestBody EmailVerificationRequest verificationRequest) {
         String email = verificationRequest.email();
         String verificationCode = verificationRequest.code();
         emailVerificationService.verifyEmail(email, verificationCode);
