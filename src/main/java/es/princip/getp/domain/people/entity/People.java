8a462280c807a8833880116046b0b95336ba34ba
wlgns12370 <ahffk12370@naver.com>
2024-01-13 15:29:30 +0900

    GETP-75 feat: 피플 정보 엔티티 구현

---
+++ src/main/java/es/princip/getp/domain/people/entity/People.java
@@ -0,0 +1,60 @@
+package es.princip.getp.domain.people.entity;
+
+import es.princip.getp.domain.base.BaseTimeEntity;
+import es.princip.getp.domain.member.entity.Member;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "people")
+public class People extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "people_id")
+    private Long peopleId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_type")
+    private PeopleRoleType roleType;
+
+    @Column(name = "profile_image_uri")
+    private String profileImageUri;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    private Member member;
+
+    @Builder
+    public People(String name, String email, String phoneNumber, PeopleRoleType roleType, 
+            String profileImageUri, String accountNumber, Member member) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.roleType = roleType;
+        this.profileImageUri = profileImageUri;
+        this.accountNumber = accountNumber;
+        this.member = member;
+    }
+}
