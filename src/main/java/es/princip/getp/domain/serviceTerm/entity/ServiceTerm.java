76b7730e3b9ec23ce91617736fa3c96acc93167d
Shin Changyu <scv1702@naver.com>
2024-01-14 15:59:55 +0900

    GETP-56 fix: Proxy와 Lazy Loading을 위해 ServiceTerm과 ServiceTermAgreement 엔티티의 protected 기본 생성자 생성

--- src/main/java/es/princip/getp/domain/serviceTerm/entity/ServiceTerm.java
+++ src/main/java/es/princip/getp/domain/serviceTerm/entity/ServiceTerm.java
@@ -7,13 +7,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "service_term")
 public class ServiceTerm extends BaseTimeEntity {
     @Id
