f079e9cf80b34869e8d8eb4205879f7341f06f61
wlgns12370 <ahffk12370@naver.com>
2024-01-15 17:06:59 +0900

    GETP-75 feat: 피플 정보 컨트롤러 구현

---
+++ src/main/java/es/princip/getp/domain/people/controller/PeopleController.java
@@ -0,0 +1,56 @@
+package es.princip.getp.domain.people.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import es.princip.getp.domain.member.entity.Member;
+import es.princip.getp.domain.people.dto.request.CreatePeopleRequestDTO;
+import es.princip.getp.domain.people.dto.response.PeopleResponseDTO;
+import es.princip.getp.domain.people.service.PeopleService;
+import es.princip.getp.global.security.details.PrincipalDetails;
+import es.princip.getp.global.util.ApiResponse;
+import es.princip.getp.global.util.ApiResponse.ApiSuccessResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/people")
+@RequiredArgsConstructor
+public class PeopleController {
+    private final PeopleService peopleService;
+
+    /**
+     * 피플 계정 생성
+     * 
+     * @param CreatePeopleRequestDTO 생성할 피플 정보
+     * @return PeopleResponseDTO 생성 완료된 피플 정보
+     */
+    @PostMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiSuccessResult<PeopleResponseDTO>> create(
+            @RequestBody @Valid CreatePeopleRequestDTO request,
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Member member = principalDetails.getMember();
+        PeopleResponseDTO response = peopleService.create(member, request);
+        return ResponseEntity.ok().body(ApiResponse.success(HttpStatus.CREATED, response));
+    }
+
+    /**
+     * 피플 상세 조회
+     * 
+     * @param peopleId 피플 ID
+     * @return PeopleResponseDTO 피플 ID에 해당되는 피플 정보
+     */
+    @GetMapping("/{peopleId}")
+    public ResponseEntity<ApiSuccessResult<PeopleResponseDTO>> getPeopleByPeopleId(@PathVariable Long peopleId) {
+        PeopleResponseDTO response = peopleService.getResponseByPeopleId(peopleId);
+        return ResponseEntity.ok().body(ApiResponse.success(HttpStatus.CREATED, response));
+    }
+}
