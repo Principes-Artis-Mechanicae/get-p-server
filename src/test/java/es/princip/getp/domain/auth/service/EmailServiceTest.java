5510663b0e3691f8200aed6e9976355747b6b825
Shin Changyu <scv1702@naver.com>
2024-01-17 19:49:16 +0900

    GETP-64 test: 이메일 및 이메일 인증 단위 테스트를 @Nested로 가독성 향상

--- src/test/java/es/princip/getp/domain/auth/service/EmailServiceTest.java
+++ src/test/java/es/princip/getp/domain/auth/service/EmailServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,46 +15,49 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import es.princip.getp.domain.auth.exception.EmailErrorCode;
-import es.princip.getp.domain.auth.service.GmailService;
 import es.princip.getp.global.exception.BusinessLogicException;
 
 @ExtendWith(MockitoExtension.class)
 class EmailServiceTest {
     @Mock
     private JavaMailSender emailSender;
-    
+
     @InjectMocks
     private GmailService emailService;
 
-    @DisplayName("이메일 전송")
-    @Test
-    void sendEmail() {
-        String email = "test@example.com";
-        String title = "title";
-        String text = "text";
-        SimpleMailMessage emailForm = new SimpleMailMessage();
-        emailForm.setTo(email);
-        emailForm.setSubject(title);
-        emailForm.setText(text);
+    @Nested
+    @DisplayName("sendEmail()은")
+    class SendEmail {
+        @DisplayName("이메일을 전송한다.")
+        @Test
+        void sendEmail() {
+            String email = "test@example.com";
+            String title = "title";
+            String text = "text";
+            SimpleMailMessage emailForm = new SimpleMailMessage();
+            emailForm.setTo(email);
+            emailForm.setSubject(title);
+            emailForm.setText(text);
 
-        emailService.sendEmail(email, title, text);
+            emailService.sendEmail(email, title, text);
 
-        verify(emailSender, times(1)).send(eq(emailForm));
-    }
+            verify(emailSender, times(1)).send(eq(emailForm));
+        }
 
-    @DisplayName("잘못된 이메일 주소인 경우 예외 발생")
-    @Test
-    void sendEmail_WhenEmailIsWrong_ShouldThrowException() {
-        String email = "testexample.com";
-        String title = "title";
-        String text = "text";
-        SimpleMailMessage emailForm = new SimpleMailMessage();
-        emailForm.setTo(email);
-        emailForm.setSubject(title);
-        emailForm.setText(text);
+        @DisplayName("잘못된 이메일 주소인 경우 실패한다.")
+        @Test
+        void sendEmail_WhenEmailIsWrong_ShouldThrowException() {
+            String email = "testexample.com";
+            String title = "title";
+            String text = "text";
+            SimpleMailMessage emailForm = new SimpleMailMessage();
+            emailForm.setTo(email);
+            emailForm.setSubject(title);
+            emailForm.setText(text);
 
-        BusinessLogicException exception = 
-            assertThrows(BusinessLogicException.class, () ->  emailService.sendEmail(email, title, text));
-        assertEquals(exception.getCode(), EmailErrorCode.WRONG_EMAIL.name());
+            BusinessLogicException exception =
+                assertThrows(BusinessLogicException.class, () ->  emailService.sendEmail(email, title, text));
+            assertEquals(exception.getCode(), EmailErrorCode.WRONG_EMAIL.name());
+        }
     }
 }
