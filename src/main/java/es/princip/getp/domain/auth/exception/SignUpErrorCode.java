a7e75e4532624633d3da1c060e4c20b0ec332c93
Shin Changyu <scv1702@naver.com>
2024-01-17 19:14:17 +0900

    GETP-56 fix: 회원 가입 시 필수 서비스 약관에 동의하지 않으면 회원 엔티티를 생성하지 않도록 변경

--- src/main/java/es/princip/getp/domain/auth/exception/SignUpErrorCode.java
+++ src/main/java/es/princip/getp/domain/auth/exception/SignUpErrorCode.java
@@ -7,6 +7,7 @@ public enum SignUpErrorCode implements ErrorCode {
     WRONG_EMAIL(HttpStatus.BAD_REQUEST, "잘못된 이메일 주소"),
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 형식"),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일 주소"),
+    NOT_AGREED_REQUIRED_SERVICE_TERM(HttpStatus.CONFLICT, "필수 서비스 약관 미동의"),
     NOT_VERIFIED_EMAIL(HttpStatus.CONFLICT, "이메일 인증 미실시");
 
     private final HttpStatus status;
