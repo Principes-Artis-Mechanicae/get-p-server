65b501362791a4ceb6e2ba03fcbef28cd091d22c
lsmin3388 <lsmin3388_@naver.com>
2024-01-20 16:20:19 +0900

    GETP-71 feat: Refresh Token으로 Access Token 재발급 구현

--- src/main/java/es/princip/getp/domain/auth/dto/response/Token.java
+++ src/main/java/es/princip/getp/domain/auth/dto/response/Token.java
@@ -2,10 +2,13 @@ package es.princip.getp.domain.auth.dto.response;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public record Token(
     @NotNull String grantType,
-    @NotNull String accessToken
+    @NotNull String accessToken,
+    @NotNull String refreshToken
 ) {
 }
