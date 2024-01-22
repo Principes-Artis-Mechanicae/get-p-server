d72531b5959a2e53652c6361a3a6c12155b1fce9
wlgns12370 <ahffk12370@naver.com>
2024-01-23 02:09:21 +0900

    GETP-75 test: PeopleService 기본 CRUD 테스트 코드 구현

--- src/test/java/es/princip/getp/fixture/PeopleFixture.java
+++ src/test/java/es/princip/getp/fixture/PeopleFixture.java
@@ -2,14 +2,13 @@ package es.princip.getp.fixture;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import es.princip.getp.domain.member.entity.Member;
 import es.princip.getp.domain.people.dto.request.CreatePeopleRequest;
+import es.princip.getp.domain.people.dto.request.UpdatePeopleRequest;
 import es.princip.getp.domain.people.entity.People;
 import es.princip.getp.domain.people.entity.PeopleRoleType;
 
 public class PeopleFixture {
-
     public static String NAME = "겟피";
     public static String EMAIL = "getp@princip.es";
     public static String PHONE_NUMBER = "010-1234-5678";
@@ -18,7 +17,11 @@ public class PeopleFixture {
     public static String ACCOUNT_NUMBER = "3332-112-12-12";
 
     public static CreatePeopleRequest createPeopleRequest() {
-        return new CreatePeopleRequest(NAME, EMAIL, PHONE_NUMBER, NAME, EMAIL, ACCOUNT_NUMBER);
+        return new CreatePeopleRequest(NAME, EMAIL, PHONE_NUMBER, ROLE_TYPE, PROFILE_IMAGE_URI, ACCOUNT_NUMBER);
+    }
+
+    public static UpdatePeopleRequest updatePeopleRequest() {
+        return new UpdatePeopleRequest(NAME+".update", EMAIL, PHONE_NUMBER, ROLE_TYPE, PROFILE_IMAGE_URI, ACCOUNT_NUMBER);
     }
 
     public static People createPeopleByMember(Member member) {
