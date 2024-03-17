1fec839ecc967b4f4a91932c5d4b45965c322a89
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2024-03-17 23:42:12 +0900

    GETP-101 feat: 피플 프로필-포트폴리오 수정 API 구현 (#40)
    * GETP-101 feat: 피플 프로필 포트폴리오 수정 구현
    
    * GETP-101 test: 피플 프로필 포트폴리오 수정 테스트 코드 추가
    
    * GETP-101 fix: 이메일 인증 코드를 중복해서 전송하는 경우 수정

--- src/main/java/es/princip/getp/domain/people/dto/request/UpdatePeopleProfileRequest.java
+++ src/main/java/es/princip/getp/domain/people/dto/request/UpdatePeopleProfileRequest.java
@@ -1,9 +1,11 @@
 package es.princip.getp.domain.people.dto.request;
 
 import java.util.List;
-
+import es.princip.getp.domain.people.dto.PortfolioForm;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdatePeopleProfileRequest(@NotNull String introduction, @NotNull String activityArea,
-                                            @NotNull List<String> techStacks, @NotNull String education, @NotNull List<String> hashtags) {
+                                            @NotNull List<String> techStacks, @NotNull String education, @NotNull List<String> hashtags,
+                                            @Valid List<PortfolioForm> portfolios) {
 }
