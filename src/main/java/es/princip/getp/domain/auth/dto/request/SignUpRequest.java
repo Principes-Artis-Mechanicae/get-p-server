759d00455afeefc4fc9613dd583a500e87eccdaf
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-04-01 16:37:08 +0900

    GETP-56 fix: 사용자 회원 가입 시 선택할 수 있는 회원 유형을 피플 또는 의뢰자로 제한 (#49)

--- src/main/java/es/princip/getp/domain/auth/dto/request/SignUpRequest.java
+++ src/main/java/es/princip/getp/domain/auth/dto/request/SignUpRequest.java
@@ -2,8 +2,8 @@ package es.princip.getp.domain.auth.dto.request;
 
 import es.princip.getp.domain.auth.dto.validator.annotation.Password;
 import es.princip.getp.domain.member.domain.enums.MemberType;
+import es.princip.getp.domain.member.validator.annotation.UserMemberType;
 import es.princip.getp.domain.serviceTerm.dto.reqeust.ServiceTermAgreementRequest;
-import es.princip.getp.global.validator.annotation.Enum;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
@@ -13,6 +13,6 @@ public record SignUpRequest(
     @NotNull @Password String password,
     @NotNull String verificationCode,
     @NotNull List<ServiceTermAgreementRequest> serviceTerms,
-    @Enum MemberType memberType
+    @UserMemberType MemberType memberType
 ) {
 }
