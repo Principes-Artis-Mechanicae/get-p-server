fd6f1d8751a795812236bc4ec006d6ab86d4e1ed
wlgns12370 <ahffk12370@naver.com>
2024-04-03 23:54:27 +0900

    GETP-112 feat: 피플 좋아요 기능 레포지토리 구현

---
+++ src/main/java/es/princip/getp/domain/people/repository/PeopleLikeRepository.java
@@ -0,0 +1,9 @@
+package es.princip.getp.domain.people.repository;
+
+import es.princip.getp.domain.people.domain.entity.PeopleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeopleLikeRepository extends JpaRepository<PeopleLike, Long> {
+
+    boolean existsByClient_ClientIdAndPeople_PeopleId(Long clientId, Long peopleId);
+}
