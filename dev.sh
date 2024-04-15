a783c9d328d0e5c2df04c7bb37d9c4c9ff651ddf
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-04-15 20:01:02 +0900

    fix: env.sh 분리로 인한 환경 변수 인식 오류 수정 (#54)

--- dev.sh
+++ dev.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-./env.sh .env.dev
+ENV_FILE=".env.dev"
+
+echo "[env.sh] environment variable file: $ENV_FILE"
+
+# 환경 변수 파일이 존재하는지 확인
+if [ -f "$ENV_FILE" ]; then
+    # 파일에서 각 줄을 읽어옴
+    while IFS= read -r line || [ -n "$line" ]; do
+        # 주석이 아닌 줄이고, key=value 형태인 경우에만 처리
+        if [[ $line != \#* && $line == *"="* ]]; then
+            # key와 value를 분리
+            key="${line%%=*}"
+            value="${line#*=}"
+            value=$(sed 's/^"\|"$//g' <<< "$value")
+            # 환경 변수로 등록
+            export "$key"="$value"
+        fi
+    done < "$ENV_FILE"
+else
+    echo "[env.sh] $ENV_FILE not found."
+fi
+
+echo "[env.sh] environment variable load complete"
+
 ./gradlew clean build
 sudo docker compose --env-file .env.dev up -d --build
+
 echo "[dev.sh] deploy complete"
