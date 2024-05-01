36d67d86b0bef18bf2145c5e863bcc92464a97da
Changyu Shin <scv1702@naver.com>
2024-05-01 12:22:38 +0900

    feat: Spring REST Docs 헬퍼 클래스 구현

--- src/main/java/es/princip/getp/domain/auth/annotation/Password.java
+++ src/main/java/es/princip/getp/domain/auth/annotation/Password.java
@@ -1,4 +1,4 @@
-package es.princip.getp.domain.auth.dto.validator.annotation;
+package es.princip.getp.domain.auth.annotation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
@@ -9,13 +9,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Pattern(
-    message = "{validation.constraints.Password.message}",
     regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*_+=/])[A-Za-z\\d!@#$%^&*_+=/]{8,20}$")
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Constraint(validatedBy = {})
 public @interface Password {
-    String message() default "";
+    String message() default "{validation.constraints.Password.message}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
