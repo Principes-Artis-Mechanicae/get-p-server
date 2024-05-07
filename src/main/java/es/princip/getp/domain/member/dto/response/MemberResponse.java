fcb7171597265c31d0fef5ac4b53e221ef203780
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-05-07 22:07:48 +0900

    feat: 내 사용자 정보 조회 기능 구현 (#60)
    * chore: 인증 코드 만료 시간 환경 변수명을 VERIFICATION_CODE_EXPIRE_TIME으로 변경
    
    * feat: 내 사용자 정보 조회 기능 구현
    
    * test: 내 사용자 정보 조회 기능 테스트 작성
    
    * chore: static 파일 gitignore에 추가

--- src/main/java/es/princip/getp/domain/member/dto/response/MemberResponse.java
+++ src/main/java/es/princip/getp/domain/member/dto/response/MemberResponse.java
@@ -1,22 +1,43 @@
 package es.princip.getp.domain.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import es.princip.getp.domain.member.domain.entity.Member;
 import es.princip.getp.domain.member.domain.enums.MemberType;
+
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MemberResponse(
     Long memberId,
     String email,
+    String nickname,
     MemberType memberType,
     LocalDateTime createdAt,
-    LocalDateTime updatedAt) {
+    LocalDateTime updatedAt
+) {
 
     public static MemberResponse from(final Member member) {
         return new MemberResponse(
             member.getMemberId(),
             member.getEmail(),
+            null,
             member.getMemberType(),
             member.getCreatedAt(),
-            member.getUpdateAt());
+            member.getUpdatedAt()
+        );
+    }
+
+    public static MemberResponse of(
+        final Member member,
+        final String nickname
+    ) {
+        return new MemberResponse(
+            member.getMemberId(),
+            member.getEmail(),
+            nickname,
+            member.getMemberType(),
+            member.getCreatedAt(),
+            member.getUpdatedAt()
+        );
     }
 }
--- src/test/java/es/princip/getp/global/mock/WithCustomMockUser.java
+++ src/test/java/es/princip/getp/global/mock/WithCustomMockUser.java
@@ -1,14 +1,16 @@
 package es.princip.getp.global.mock;
 
 import es.princip.getp.domain.member.domain.enums.MemberType;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import org.springframework.security.test.context.support.WithSecurityContext;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
 public @interface WithCustomMockUser {
 
+    long memberId() default 1L;
     String email() default "test@example.com";
     MemberType memberType() default MemberType.ROLE_PEOPLE;
 }
