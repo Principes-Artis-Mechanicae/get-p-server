7e8eb60bdf0228fd34a01fe8ceffbc5200f4f7d1
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-05-10 13:53:04 +0900

    fix: 내 회원 정보 조회 시 피플 또는 의뢰자 정보를 등록하지 않은 경우 404를 반환하는 오류 수정 (#61)

--- src/main/java/es/princip/getp/domain/member/controller/MemberController.java
+++ src/main/java/es/princip/getp/domain/member/controller/MemberController.java
@@ -1,10 +1,7 @@
 package es.princip.getp.domain.member.controller;
 
-import es.princip.getp.domain.client.service.ClientService;
 import es.princip.getp.domain.member.domain.entity.Member;
-import es.princip.getp.domain.member.domain.enums.MemberType;
 import es.princip.getp.domain.member.dto.response.MemberResponse;
-import es.princip.getp.domain.people.service.PeopleService;
 import es.princip.getp.global.security.details.PrincipalDetails;
 import es.princip.getp.global.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -23,21 +20,12 @@ import static es.princip.getp.global.util.ApiResponse.ApiSuccessResult;
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final PeopleService peopleService;
-    private final ClientService clientService;
-
     @GetMapping("/me")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<ApiSuccessResult<MemberResponse>> getMyMember(
         @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
         Member member = principalDetails.getMember();
-        Long memberId = member.getMemberId();
-        String email = member.getEmail();
-        MemberType memberType = member.getMemberType();
-        String nickname = memberType.isClient() ? clientService.getByMemberId(memberId).getNickname() :
-                          memberType.isPeople() ? peopleService.getByMemberId(memberId).getNickname() :
-                          email.substring(0, email.indexOf('@'));
-        return ResponseEntity.ok().body(ApiResponse.success(HttpStatus.OK, MemberResponse.of(member, nickname)));
+        return ResponseEntity.ok().body(ApiResponse.success(HttpStatus.OK, MemberResponse.from(member)));
     }
 }
