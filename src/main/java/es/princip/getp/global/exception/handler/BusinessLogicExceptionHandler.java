bfe927794f5ec069ec88e3344ef537b688ba9899
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-06-09 13:06:27 +0900

    GETP-141 test: 로그인 컨트롤러 단위 테스트 및 API 문서 작성 (#66)
    * GETP-141 rename: fixture 클래스를 각 도메인의 fixture 패키지로 이동
    
    * GETP-141 fix: bean validation 시 커스텀 메시지가 출력되지 않는 오류 수정
    
    * GETP-141 test: 로그인 컨트롤러 단위 테스트 및 API 문서 작성

--- src/main/java/es/princip/getp/global/exception/handler/BusinessLogicExceptionHandler.java
+++ src/main/java/es/princip/getp/global/exception/handler/BusinessLogicExceptionHandler.java
@@ -1,22 +1,21 @@
 package es.princip.getp.global.exception.handler;
 
+import es.princip.getp.global.exception.BusinessLogicException;
 import es.princip.getp.global.exception.ErrorCode;
-import org.springframework.http.HttpStatus;
+import es.princip.getp.global.util.ApiResponse;
+import es.princip.getp.global.util.ApiResponse.ApiErrorResult;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import es.princip.getp.global.exception.BusinessLogicException;
-import es.princip.getp.global.util.ApiResponse;
-import es.princip.getp.global.util.ApiResponse.ApiErrorResult;
 
 @RestControllerAdvice
 public class BusinessLogicExceptionHandler {
 
     @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity<ApiErrorResult> businessLogicException(
-        final BusinessLogicException businessLogicException) {
-        ErrorCode errorCode = businessLogicException.getErrorCode();
-        HttpStatus status = errorCode.status();
-        return ResponseEntity.status(status).body(ApiResponse.error(errorCode));
+    public ResponseEntity<ApiErrorResult> validationException(
+        final BusinessLogicException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        return ResponseEntity.status(errorCode.status())
+            .body(ApiResponse.error(errorCode.status(), errorCode.description()));
     }
 }
