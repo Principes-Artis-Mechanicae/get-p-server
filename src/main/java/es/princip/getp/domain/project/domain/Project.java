9a2d4792e5eca3f499733b569a084791aefc1d4e
Changyu Shin <scv1702@naver.com>
2024-07-12 21:55:35 +0900

    refactor: 프로젝트 프로필 애그리거트를 루트 애그리거트를 통해서만 접근 가능하도록 수정

--- src/main/java/es/princip/getp/domain/project/domain/Project.java
+++ src/main/java/es/princip/getp/domain/project/domain/Project.java
@@ -6,9 +6,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@Getter
 @Entity
 @Builder
 @AllArgsConstructor
@@ -19,40 +19,49 @@ public class Project extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
+    @Getter
     private Long projectId;
 
     // 제목
     @Column(name = "title")
+    @Getter
     private String title;
 
     // 금액
     @Column(name = "payment")
+    @Getter
     private Long payment;
 
     // 지원자 모집 마감일
     @Embedded
+    @Getter
     private ApplicationDeadline applicationDeadline;
 
     // 예상 작업 기간
     @Embedded
+    @Getter
     private EstimatedDuration estimatedDuration;
 
     // 상세 설명
     @Column(name = "description")
+    @Getter
     private String description;
 
     // 미팅 유형
     @Column(name = "meeting_type")
     @Enumerated(EnumType.STRING)
+    @Getter
     private MeetingType meetingType;
 
     // 의뢰자
     @Column(name = "client_id")
+    @Getter
     private Long clientId;
 
     // 관심 수
     //TODO: 관심 수를 계산하는 로직 구현 필요
     @Transient
+    @Getter
     private int interestsCount;
 
     // 첨부 파일 목록
@@ -76,5 +85,13 @@ public class Project extends BaseTimeEntity {
             throw new IllegalArgumentException("Application is closed");
         }
         applications.add(application);
+    }
+
+    public List<AttachmentFile> getAttachmentFiles() {
+        return Collections.unmodifiableList(attachmentFiles);
+    }
+
+    public List<Hashtag> getHashtags() {
+        return Collections.unmodifiableList(hashtags);
     }
 }
