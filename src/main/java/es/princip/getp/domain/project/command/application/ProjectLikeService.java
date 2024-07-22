f2bddd064be94d758ecb77bbfd4d0200e59f9db3
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-07-22 12:11:42 +0900

    refactor: 프로젝트 도메인에서 ErrorCode Enum 제거 (#82)

--- src/main/java/es/princip/getp/domain/project/command/application/ProjectLikeService.java
+++ src/main/java/es/princip/getp/domain/project/command/application/ProjectLikeService.java
@@ -3,6 +3,7 @@ package es.princip.getp.domain.project.command.application;
 import es.princip.getp.domain.people.command.domain.People;
 import es.princip.getp.domain.people.command.domain.PeopleRepository;
 import es.princip.getp.domain.project.command.domain.ProjectRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,9 +19,9 @@ public class ProjectLikeService {
     @Transactional
     public void like(Long memberId, Long projectId) {
         if (!projectRepository.existsById(projectId)) {
-            throw new IllegalArgumentException("Project does not exist");
+            throw new EntityNotFoundException("해당 프로젝트가 존재하지 않습니다.");
         }
-        People people = peopleRepository.findByMemberId(memberId).orElseThrow();
+        final People people = peopleRepository.findByMemberId(memberId).orElseThrow();
         people.likeProject(projectId);
     }
 }
--- src/test/java/es/princip/getp/domain/project/command/presentation/ProjectLikeControllerTest.java
+++ src/test/java/es/princip/getp/domain/project/command/presentation/ProjectLikeControllerTest.java
@@ -2,10 +2,7 @@ package es.princip.getp.domain.project.command.presentation;
 
 import es.princip.getp.domain.member.command.domain.model.MemberType;
 import es.princip.getp.domain.project.command.application.ProjectLikeService;
-import es.princip.getp.domain.project.exception.ProjectErrorCode;
-import es.princip.getp.domain.project.exception.ProjectLikeErrorCode;
 import es.princip.getp.infra.annotation.WithCustomMockUser;
-import es.princip.getp.infra.exception.BusinessLogicException;
 import es.princip.getp.infra.support.AbstractControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -16,7 +13,6 @@ import org.springframework.http.MediaType;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProjectLikeController.class)
@@ -42,18 +38,6 @@ public class ProjectLikeControllerTest extends AbstractControllerTest {
                 .andExpect(status().isCreated());
         }
 
-        @DisplayName("이미 좋아요를 누른 프로젝트에 다시 좋아요를 누를 수 없다.")
-        @WithCustomMockUser(memberType = MemberType.ROLE_PEOPLE)
-        @Test
-        void like_WhenProjectIsAlreadyLiked_ShouldBeFailed() throws Exception {
-            willThrow(new BusinessLogicException(ProjectLikeErrorCode.ALREADY_LIKED))
-                .given(projectLikeService).like(any(), any());
-
-            mockMvc.perform(post("/projects/{projectId}/likes", projectId)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isConflict());
-        }
-
         @DisplayName("의뢰자는 프로젝트에 좋아요를 누를 수 없다.")
         @WithCustomMockUser(memberType = MemberType.ROLE_CLIENT)
         @Test
@@ -61,18 +45,6 @@ public class ProjectLikeControllerTest extends AbstractControllerTest {
             mockMvc.perform(post("/projects/{projectId}/likes", projectId)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isForbidden());
-        }
-
-        @DisplayName("존재하지 않는 프로젝트에 좋아요를 누를 수 없다.")
-        @WithCustomMockUser(memberType = MemberType.ROLE_PEOPLE)
-        @Test
-        void like_WhenProjectIsNotFound_ShouldBeFailed() throws Exception {
-            willThrow(new BusinessLogicException(ProjectErrorCode.NOT_FOUND))
-                .given(projectLikeService).like(any(), any());
-
-            mockMvc.perform(post("/projects/{projectId}/likes", projectId)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
         }
     }
 }
