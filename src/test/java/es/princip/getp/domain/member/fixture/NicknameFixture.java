f424c57499368a218eb33a80847dccd900725dc1
Changyu Shin <scv1702@naver.com>
2024-07-24 19:19:58 +0900

    test: 프로젝트 목록 조회 컨트롤러 단위 테스트 작성

--- src/test/java/es/princip/getp/domain/member/fixture/NicknameFixture.java
+++ src/test/java/es/princip/getp/domain/member/fixture/NicknameFixture.java
@@ -3,7 +3,7 @@ package es.princip.getp.domain.member.fixture;
 import es.princip.getp.domain.member.command.domain.model.Nickname;
 
 public class NicknameFixture {
-    public static final String NICKNAME = "nickname";
+    public static final String NICKNAME = "닉네임";
 
     public static Nickname nickname() {
         return Nickname.of(NICKNAME);
