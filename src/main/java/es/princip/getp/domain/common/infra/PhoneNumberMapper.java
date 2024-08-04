e5c1e166cf71f5210f8b2bf6b6419bfea46a6a28
Changyu Shin <scv1702@naver.com>
2024-08-04 19:36:12 +0900

    GETP-157 fix: PhoneNumberMapper의 NPE 오류 수정

--- src/main/java/es/princip/getp/domain/common/infra/PhoneNumberMapper.java
+++ src/main/java/es/princip/getp/domain/common/infra/PhoneNumberMapper.java
@@ -6,5 +6,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface PhoneNumberMapper {
 
-    PhoneNumber mapToPhoneNumber(String phoneNumber);
+    PhoneNumber mapToPhoneNumber(String value);
 }
