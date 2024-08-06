d0708b6d9fe3e9f4863d7ac82cfc7f1f098a2a8c
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-08-06 18:46:00 +0900

    GETP-172 feat: 내 피플 정보 조회 DTO에 전화번호 필드 추가 (#110)

--- src/test/java/es/princip/getp/domain/people/query/presentation/description/response/MyPeopleResponseDescription.java
+++ src/test/java/es/princip/getp/domain/people/query/presentation/description/response/MyPeopleResponseDescription.java
@@ -5,13 +5,14 @@ import org.springframework.restdocs.payload.FieldDescriptor;
 import static es.princip.getp.infra.util.FieldDescriptorHelper.getDescriptor;
 import static org.springframework.restdocs.snippet.Attributes.key;
 
-public class PeopleResponseDescription {
+public class MyPeopleResponseDescription {
 
     public static FieldDescriptor[] description() {
         return new FieldDescriptor[] {
             getDescriptor("peopleId", "피플 ID"),
             getDescriptor("email", "이메일"),
             getDescriptor("nickname", "닉네임"),
+            getDescriptor("phoneNumber", "전화번호"),
             getDescriptor("profileImageUri", "프로필 이미지 URI"),
             getDescriptor("peopleType", "피플 유형")
                 .attributes(key("format").value("TEAM, INDIVIDUAL")),
