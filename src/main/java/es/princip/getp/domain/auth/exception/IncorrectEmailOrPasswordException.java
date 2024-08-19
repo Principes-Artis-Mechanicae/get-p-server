a92bf3395a24faf5b467bb6b1428833271c8633c
Changyu Shin <scv1702@naver.com>
2024-08-19 12:46:47 +0900

    GETP-192 refactor: AuthService에서 ErrorCode에 대한 의존성 제거

---
+++ src/main/java/es/princip/getp/domain/auth/exception/IncorrectEmailOrPasswordException.java
@@ -0,0 +1,14 @@
+package es.princip.getp.domain.auth.exception;
+
+import es.princip.getp.infra.exception.BusinessLogicException;
+import es.princip.getp.infra.exception.ErrorDescription;
+
+public class IncorrectEmailOrPasswordException extends BusinessLogicException {
+
+    private static final String code = "INCORRECT_EMAIL_OR_PASSWORD";
+    private static final String message = "이메일 또는 비밀번호가 일치하지 않습니다.";
+
+    public IncorrectEmailOrPasswordException() {
+        super(ErrorDescription.of(code, message));
+    }
+}
