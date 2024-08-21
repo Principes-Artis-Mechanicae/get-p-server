b42c11f19d8075206af2ee49cc53927c09ca7cc1
Changyu Shin <scv1702@naver.com>
2024-08-22 01:59:41 +0900

    GETP-203 refactor: Address를 JPA VO와 도메인 VO로 분리

---
+++ src/main/java/es/princip/getp/domain/client/model/Address.java
@@ -0,0 +1,37 @@
+package es.princip.getp.domain.client.model;
+
+import es.princip.getp.domain.BaseModel;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
+@EqualsAndHashCode(callSuper = false)
+public class Address extends BaseModel {
+
+    @NotBlank private final String zipcode;
+    @NotBlank private final String street;
+    private final String detail;
+
+    public Address(
+        final String zipcode,
+        final String street,
+        final String detail
+    ) {
+        this.zipcode = zipcode;
+        this.street = street;
+        this.detail = detail;
+
+        validate();
+    }
+
+    @Override
+    public String toString() {
+        return Stream.of(zipcode, street, detail)
+            .filter(part -> part != null && !part.trim().isEmpty())
+            .collect(Collectors.joining(" "));
+    }
+}
