93fdd65b5e7020ade0c10b4a48d0ea064098bf08
Changyu Shin <scv1702@naver.com>
2024-08-21 22:12:06 +0900

    GETP-200 refactor: member 컴포넌트의 profile image 기능을 application 모듈로 리팩토링

--- src/main/java/es/princip/getp/application/member/port/in/ChangeProfileImageUseCase.java
+++
@@ -1,8 +0,0 @@
-package es.princip.getp.application.member.port.in;
-
-import es.princip.getp.application.member.command.ChangeProfileImageCommand;
-
-public interface ChangeProfileImageUseCase {
-
-    String changeProfileImage(ChangeProfileImageCommand command);
-}
