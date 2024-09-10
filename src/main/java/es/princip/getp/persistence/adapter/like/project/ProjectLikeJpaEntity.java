470021c1da82b141c165362b8da8071085334efd
Changyu Shin <scv1702@naver.com>
2024-09-10 15:09:47 +0900

    GETP-224 fix: people_like 테이블과 project_like 테이블의 primary key 이름을 JPA 매핑에 맞게 변경

--- src/main/java/es/princip/getp/persistence/adapter/like/project/ProjectLikeJpaEntity.java
+++ src/main/java/es/princip/getp/persistence/adapter/like/project/ProjectLikeJpaEntity.java
@@ -1,21 +1,24 @@
 package es.princip.getp.persistence.adapter.like.project;
 
-import es.princip.getp.persistence.adapter.BaseTimeJpaEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @Table(name = "project_like")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ProjectLikeJpaEntity extends BaseTimeJpaEntity {
+public class ProjectLikeJpaEntity {
 
     @Id
+    @Column(name = "project_like_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -25,18 +28,20 @@ public class ProjectLikeJpaEntity extends BaseTimeJpaEntity {
     @Column(name = "project_id")
     private Long projectId;
 
+    @CreatedDate
+    @Column(columnDefinition = "TIMESTAMP")
+    protected LocalDateTime createdAt;
+
     @Builder
     public ProjectLikeJpaEntity(
         final Long id,
         final Long peopleId,
         final Long projectId,
-        final LocalDateTime createdAt,
-        final LocalDateTime updatedAt
+        final LocalDateTime createdAt
     ) {
-        super(createdAt, updatedAt);
-
         this.id = id;
         this.peopleId = peopleId;
         this.projectId = projectId;
+        this.createdAt = createdAt;
     }
 }
