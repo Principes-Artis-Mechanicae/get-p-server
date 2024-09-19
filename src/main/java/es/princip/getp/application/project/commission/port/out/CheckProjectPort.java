10772c35cfb6000af672836596675f1dbaf47f09
Changyu Shin <scv1702@naver.com>
2024-09-19 18:30:54 +0900

    GETP-238 refactor: domain 모듈이 ProjectApplicationId 클래스를 사용하도록 변경

--- src/main/java/es/princip/getp/application/project/commission/port/out/CheckProjectPort.java
+++
@@ -1,5 +0,0 @@
-package es.princip.getp.application.project.commission.port.out;
-
-public interface CheckProjectPort {
-    
-}
--- src/main/java/es/princip/getp/persistence/adapter/project/meeting/ProjectMeetingQueryDslRepository.java
+++
@@ -1,9 +0,0 @@
-package es.princip.getp.persistence.adapter.project.meeting;
-
-import es.princip.getp.domain.member.model.MemberId;
-import es.princip.getp.domain.project.commission.model.ProjectId;
-
-interface ProjectMeetingQueryDslRepository {
-
-    boolean existsApplicantBy(ProjectId projectId, MemberId memberId);
-}
