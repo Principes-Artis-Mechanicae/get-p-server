b8fe247174f68e2b8f08a511b24a86a8970f78a8
Changyu Shin <scv1702@naver.com>
2024-09-19 17:09:34 +0900

    GETP-237 refactor: test fixture 모듈이 ProjectId 클래스를 사용하도록 변경

--- src/test/java/es/princip/getp/fixture/like/ProjectLikeFixture.java
+++ src/test/java/es/princip/getp/fixture/like/ProjectLikeFixture.java
@@ -2,9 +2,10 @@ package es.princip.getp.fixture.like;
 
 import es.princip.getp.domain.like.project.model.ProjectLike;
 import es.princip.getp.domain.people.model.PeopleId;
+import es.princip.getp.domain.project.commission.model.ProjectId;
 
 public class ProjectLikeFixture {
-    public static ProjectLike projectLike(PeopleId peopleId, Long projectId) {
+    public static ProjectLike projectLike(PeopleId peopleId, ProjectId projectId) {
         return ProjectLike.builder()
             .peopleId(peopleId)
             .projectId(projectId)
--- src/test/java/es/princip/getp/fixture/project/ProjectApplicationFixture.java
+++ src/test/java/es/princip/getp/fixture/project/ProjectApplicationFixture.java
@@ -3,6 +3,7 @@ package es.princip.getp.fixture.project;
 import es.princip.getp.domain.common.model.Duration;
 import es.princip.getp.domain.people.model.PeopleId;
 import es.princip.getp.domain.project.apply.model.ProjectApplication;
+import es.princip.getp.domain.project.commission.model.ProjectId;
 
 import java.time.LocalDate;
 
@@ -13,7 +14,7 @@ public class ProjectApplicationFixture {
 
     public static final String DESCRIPTION = "프로젝트 지원 내용";
 
-    public static ProjectApplication projectApplication(final PeopleId peopleId, final Long projectId) {
+    public static ProjectApplication projectApplication(final PeopleId peopleId, final ProjectId projectId) {
         return ProjectApplication.builder()
             .applicantId(peopleId)
             .projectId(projectId)
