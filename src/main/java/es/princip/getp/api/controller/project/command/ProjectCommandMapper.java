8fa164f2c4e52039e6b26830d7d1a3c61a538e77
Changyu Shin <scv1702@naver.com>
2024-09-30 18:41:29 +0900

    GETP-271 feat: 프로젝트 지원(팀) API 요청 DTO의 팀원 정보 필드를 teammates로 수정

--- src/main/java/es/princip/getp/api/controller/project/command/ProjectCommandMapper.java
+++ src/main/java/es/princip/getp/api/controller/project/command/ProjectCommandMapper.java
@@ -13,6 +13,7 @@ import es.princip.getp.application.project.commission.command.CommissionProjectC
 import es.princip.getp.application.project.meeting.command.ScheduleMeetingCommand;
 import es.princip.getp.domain.common.model.AttachmentFile;
 import es.princip.getp.domain.common.model.URL;
+import es.princip.getp.domain.member.model.Member;
 import es.princip.getp.domain.member.model.MemberId;
 import es.princip.getp.domain.project.commission.model.ProjectId;
 import org.mapstruct.Mapper;
@@ -26,34 +27,34 @@ import org.mapstruct.Mapping;
 abstract class ProjectCommandMapper {
 
     ApplyProjectCommand mapToCommand(
-        MemberId memberId,
+        Member member,
         ProjectId projectId,
         ApplyProjectRequest request
     ) {
         if (request instanceof ApplyProjectAsIndividualRequest req) {
-            return mapToCommand(memberId, projectId, req);
+            return mapToCommand(member, projectId, req);
         } else if (request instanceof ApplyProjectAsTeamRequest req) {
-            return mapToCommand(memberId, projectId, req);
+            return mapToCommand(member, projectId, req);
         } else {
             throw new IllegalArgumentException("올바르지 않은 프로젝트 지원 유형: " + request.getClass());
         }
     }
 
-    @Mapping(source = "request.expectedDuration", target = "data.expectedDuration")
-    @Mapping(source = "request.description", target = "data.description")
-    @Mapping(source = "request.attachmentFiles", target = "data.attachmentFiles")
+    @Mapping(source = "request.expectedDuration", target = "expectedDuration")
+    @Mapping(source = "request.description", target = "description")
+    @Mapping(source = "request.attachmentFiles", target = "attachmentFiles")
     protected abstract ApplyProjectAsIndividualCommand mapToCommand(
-        MemberId memberId,
+        Member member,
         ProjectId projectId,
         ApplyProjectAsIndividualRequest request
     );
 
-    @Mapping(source = "request.expectedDuration", target = "data.expectedDuration")
-    @Mapping(source = "request.description", target = "data.description")
-    @Mapping(source = "request.attachmentFiles", target = "data.attachmentFiles")
-    @Mapping(source = "request.teams", target = "data.teams")
+    @Mapping(source = "request.expectedDuration", target = "expectedDuration")
+    @Mapping(source = "request.description", target = "description")
+    @Mapping(source = "request.attachmentFiles", target = "attachmentFiles")
+    @Mapping(source = "request.teammates", target = "teammates")
     protected abstract ApplyProjectAsTeamCommand mapToCommand(
-        MemberId memberId,
+        Member member,
         ProjectId projectId,
         ApplyProjectAsTeamRequest request
     );
