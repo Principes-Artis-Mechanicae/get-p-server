7e74d5ba7047875429907d0878391ee36e8be66f
Changyu Shin <scv1702@naver.com>
2024-10-01 14:39:23 +0900

    GETP-292 feat: 프로젝트 카테고리에 기타 필드 추가

---
+++ src/main/resources/db/migration/V6__add_project_category_etc.sql
@@ -0,0 +1,1 @@
+alter table project modify category enum('BACKEND', 'FRONTEND', 'PROGRAM', 'WEB', 'ETC');
--- src/test/java/es/princip/getp/api/controller/project/command/description/CommissionProjectRequestDescription.java
+++ src/test/java/es/princip/getp/api/controller/project/command/description/CommissionProjectRequestDescription.java
@@ -4,6 +4,7 @@ import es.princip.getp.api.controller.project.command.dto.request.CommissionProj
 import org.springframework.restdocs.payload.FieldDescriptor;
 
 import static es.princip.getp.api.docs.FieldDescriptorHelper.getDescriptor;
+import static org.springframework.restdocs.snippet.Attributes.key;
 
 public class CommissionProjectRequestDescription {
 
@@ -18,8 +19,10 @@ public class CommissionProjectRequestDescription {
             getDescriptor("estimatedDuration.startDate", "예상 작업 시작 기간", clazz),
             getDescriptor("estimatedDuration.endDate", "예상 작업 종료 기간", clazz),
             getDescriptor("description", "상세 설명", clazz),
-            getDescriptor("meetingType", "미팅 방식", clazz),
-            getDescriptor("category", "카테고리", clazz),
+            getDescriptor("meetingType", "미팅 방식", clazz)
+                .attributes(key("format").value("IN_PERSON, REMOTE")),
+            getDescriptor("category", "카테고리", clazz)
+                .attributes(key("format").value("BACKEND, FRONTEND, PROGRAM, WEB, ETC")),
             getDescriptor("attachmentFiles[]", "첨부 파일", clazz),
             getDescriptor("hashtags[]", "해시태그", clazz)
         };
