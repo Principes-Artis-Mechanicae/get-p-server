8caaa454194bb966b50fc07350498032af8877fc
Changyu Shin <scv1702@naver.com>
2024-10-04 00:20:16 +0900

    GETP-278 test: 프로젝트 지원자 목록 조회 컨트롤러 단위 테스트 작성

--- src/test/java/es/princip/getp/api/docs/SliceResponseDescription.java
+++ src/test/java/es/princip/getp/api/docs/SliceResponseDescription.java
@@ -1,8 +1,10 @@
 package es.princip.getp.api.docs;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import static es.princip.getp.api.docs.EnumDescriptor.fieldWithEnum;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
 public class SliceResponseDescription {
@@ -14,10 +16,16 @@ public class SliceResponseDescription {
             fieldWithPath("data.sliceInfo.first").description("첫 페이지 여부"),
             fieldWithPath("data.sliceInfo.last").description("마지막 페이지 여부"),
             fieldWithPath("data.sliceInfo.empty").description("비어있는 페이지 여부"),
-            fieldWithPath("data.sliceInfo.cursor").description("다음 페이지에 대한 커서. 다음 페이지를 요청할 때 이전 페이지 응답에서 받은 커서 값을 넣어주세요."),
+            fieldWithPath("data.sliceInfo.cursor")
+                .description("다음 페이지에 대한 커서. 다음 페이지를 요청할 때 이전 페이지 응답에서 받은 커서 값을 넣어주세요."),
             fieldWithPath("data.sliceInfo.sort").description("정렬 정보").ignored(),
-            fieldWithPath("data.sliceInfo.sort.property").description("정렬 속성").optional().type(JsonFieldType.STRING),
-            fieldWithPath("data.sliceInfo.sort.direction").description("정렬 방향").optional().type(JsonFieldType.STRING)
+            fieldWithPath("data.sliceInfo.sort.property")
+                .description("정렬 속성")
+                .optional()
+                .type(JsonFieldType.STRING),
+            fieldWithEnum(Sort.Direction.class).withPath("data.sliceInfo.sort.direction")
+                .description("정렬 방향")
+                .optional()
         };
     }
 }
