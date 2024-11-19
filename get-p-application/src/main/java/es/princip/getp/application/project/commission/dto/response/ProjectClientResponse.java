a21d53b4f03ec93855da84faa34186f48069a92b
scv1702 <scv1702@naver.com>
2024-11-19 15:23:51 +0900

    GETP-330 fix: 프로젝트 조회 시 의뢰자 정보 미등록한 경우 NPE 발생 오류 해결

--- get-p-application/src/main/java/es/princip/getp/application/project/commission/dto/response/ProjectClientResponse.java
+++ get-p-application/src/main/java/es/princip/getp/application/project/commission/dto/response/ProjectClientResponse.java
@@ -1,13 +1,13 @@
 package es.princip.getp.application.project.commission.dto.response;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import es.princip.getp.application.common.dto.response.AddressResponse;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonInclude(NON_NULL)
 public record ProjectClientResponse(
-    @JsonInclude(NON_NULL) Long clientId,
+    Long clientId,
     String nickname,
     AddressResponse address
 ) {
